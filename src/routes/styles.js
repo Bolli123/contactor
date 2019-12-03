1772aff54593d137e7922949cb7b4ffeb453716c
davidfannar98 <davidfannar98@gmail.com>
2019-12-03 16:10:21 +0000

    navbar edit

---
+++ src/routes/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  right: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  icon: {
+    fontSize: 20,
+    marginLeft: 5,
+    color: '#a9a9a9',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    marginRight: 20,
+    alignItems: 'center'
+  }
+})
